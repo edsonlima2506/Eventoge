dca10d83ed9df65c393f7b2460e98c8e8032a50d
Edson Lima <edsoneduardo526@gmail.com>
2021-10-20 01:50:23 -0300

    Adiciona dados ao arquivo data.js

--- src/data.js
+++ src/data.js
@@ -6,6 +6,74 @@ const pratosInfos = [
     {
         imagem: 'https://gomesdacosta.com.br/app/uploads/2020/11/03-Feijao.png',
         nome: 'Feijão'
+    },
+    {
+        imagem: 'https://images.pexels.com/photos/618775/pexels-photo-618775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        nome: 'Carnes'
+    },
+    {
+        imagem: 'https://images.pexels.com/photos/722223/pexels-photo-722223.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        nome: 'Ovos'
+    },
+    {
+        imagem: 'https://images.pexels.com/photos/1527603/pexels-photo-1527603.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        nome: 'Macarronada'
+    },
+    {
+        imagem: 'https://images.pexels.com/photos/4079520/pexels-photo-4079520.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        nome: 'Lasanha'
+    },
+    {
+        imagem: 'https://st2.depositphotos.com/4687049/8398/i/600/depositphotos_83989334-stock-photo-deep-fried-cassava-root.jpg',
+        nome: 'Aipim Frito'
+    },
+    {
+        imagem: 'https://melepimenta.com/wp-content/uploads/2020/06/Batata-doce-Canela-gengibre-Baixa-1.jpg',
+        nome: 'Batata doce assada'
+    },
+    {
+        imagem: 'https://www.receitasdemae.com.br/wp-content/uploads/2015/06/Batata-doce-frita-380x254.jpg',
+        nome: 'Batata doce frita'
+    },
+    {
+        imagem: 'https://images.pexels.com/photos/1583884/pexels-photo-1583884.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        nome: 'Batata frita'
+    },
+    {
+        imagem: 'https://www.kitano.com.br/wp-content/uploads/2019/08/SSP_2160-Legumes-batata-cozida-tomilho-e-cebola-1.jpg',
+        nome:  'Batata assada'
+    },
+    {
+        imagem: 'https://images.pexels.com/photos/5750392/pexels-photo-5750392.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        nome: 'Beringela'
+    },
+    {
+        imagem: 'https://boaforma.abril.com.br/wp-content/uploads/sites/2/2018/05/beterraba-com-folhas.jpg?quality=90&strip=info&w=1024',
+        nome: 'Beterraba'
+    },
+    {
+        imagem: 'https://images.pexels.com/photos/47347/broccoli-vegetable-food-healthy-47347.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        nome: 'Brócolis'
+    },
+    {
+        imagem: 'https://images.pexels.com/photos/143133/pexels-photo-143133.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        nome: 'Cenoura'
+    },
+    {
+        imagem: 'https://www.infoescola.com/wp-content/uploads/2010/08/couve-flor_227987356.jpg',
+        nome: 'Couve flor'
+    },
+    {
+        imagem: 'https://organicosinbox.com.br/wp-content/uploads/2020/11/alface-crespa-organica.jpg',
+        nome: 'Alface'
+    },
+    {
+        imagem: 'https://www.infoescola.com/wp-content/uploads/2010/02/espinafre_224442694.jpg',
+        nome: 'Espinafre'
+    },
+    {
+        imagem: 'https://images.pexels.com/photos/4022093/pexels-photo-4022093.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        nome: 'Tomate'
     }
 ];
 
