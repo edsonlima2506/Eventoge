e6e96273c17856f4acc213811472d096c58ca8cb
Edson Lima <edsoneduardo526@gmail.com>
2021-10-19 23:19:01 -0300

    Adiciona conteúdo do header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import headerImage from '../images/headerImage.png';
 
 class Header extends React.Component {
     render() {
         return (
-            <section><h1>Olá. Mundo</h1></section>
+            <section className="container">
+                <span>
+                    <h1>Como está sua alimentação?</h1>
+                    <h2>Saiba agora!</h2>
+                    <button className="buttonPlay">Jogar</button>
+                </span>
+                <img src={ headerImage } alt="" className="headerImage"/>
+            </section>
         );
     }
 }
